8c028bf35530ccf1a85da9b66917ca4b79a8673a
leewilliam236 <leewilliam236@gmail.com>
2026-04-13 18:56:52 -0700

    Uploading Sample Project

---
+++ js/M7script.js
@@ -0,0 +1,220 @@
+//declare map variable globally so all functions have access
+var map;
+var minValue;
+
+function createMap(){
+    //create the map
+    map = L.map('map', {
+        center: [37.06,-121.610556],
+        zoom: 11
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //call getData function
+    getData(map);
+};
+
+function getData(map){
+    //load the data
+    fetch("data/TrainStationsM7.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            var attributes = processData(json);
+            minValue = calcMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+        })
+};
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("Year") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+};
+
+function calcMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each station
+    for(var station of data.features){
+        //loop through each year
+        for(var year = 2011; year <= 2019; year++){
+              //get population for current year
+              var value = station.properties["Year_"+ String(year)];
+              //add value to array
+              allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+function createPropSymbols(data, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//function to convert markers to circle markers and add popups
+function pointToLayer(feature, latlng,attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+    //check
+
+    //create marker options
+    var options = {
+        fillColor: getColor(feature.properties[attribute]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+    console.log(options.radius);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string starting with city...Example 2.1 line 24
+    var popupContent = "<p><b>Station Name:</b> " + feature.properties.StationName + "</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+          offset: new L.Point(0,-options.radius)
+      });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+function getColor(d){
+    return d > 200 ? '#490a0d':
+           d > 100 ? '#dd1f29':
+                     '#f3b4b7';
+}
+
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+    //set slider attributes
+    document.querySelector(".range-slider").max = 9;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    //add step buttons
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse">Reverse</button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward">Forward</button>');
+
+    //replace button content with images
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>")
+
+    var steps = document.querySelectorAll('.step');
+
+    steps.forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+            //Step 6: increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //Step 7: if past the last attribute, wrap around to first attribute
+                index = index > 9 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //Step 7: if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 9 : index;
+            };
+
+            //Step 8: update slider
+            document.querySelector('.range-slider').value = index;
+
+            //Step 9: pass new attribute to update symbols
+            updatePropSymbols(attributes[index]);
+        })
+    })
+
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){
+        //Step 6: get the new index value
+        var index = this.value;
+
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+          //access feature properties
+           var props = layer.feature.properties;
+
+           //update each feature's radius based on new attribute values
+           var radius = calcPropRadius(props[attribute]);
+           layer.setRadius(radius);
+
+           //setStyle modifies the marker's fillColor property.
+           layer.setStyle({fillColor: getColor(props[attribute])});
+           console.log(`${props[attribute]}: ${layer.defaultOptions.fillColor}`);
+
+           //add city to popup content string
+           var popupContent = "<p><b>Station Name:</b> " + props.StationName + "</p>";
+
+           //add formatted attribute to panel content string
+           var year = attribute.split("_")[1];
+           popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + "</p>";
+
+           //update popup with new content
+           popup = layer.getPopup();
+           popup.setContent(popupContent).update();
+        };
+    });
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
